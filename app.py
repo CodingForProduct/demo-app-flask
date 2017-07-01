d4c4e3627c2fb639c0a72226579ff1e9279d850f
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-01 08:36:58 -0700

    10. add array of users to teams data

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def users():
 @app.route("/users/<int:user_id>")
 def user(user_id):
     """display one user"""
+    # return all users whose id matches url user_id
     target_user = [user for user in WORKSHOP_USERS if user['id'] == user_id]
 
     return render_template('user.html', user=target_user[0])
@@ -36,7 +37,17 @@ def user(user_id):
 @app.route("/teams")
 def teams():
     """display list of teams"""
-    return render_template("teams.html", teams=WORKSHOP_TEAMS)
+    def get_team_members(team):
+        # find users whose team_id matches the given team
+        team_members = [user for user in WORKSHOP_USERS if user['team_id'] == team['id']]
+
+        # add users to team
+        return {'name': team['name'], 'id': team['id'], 'users': team_members}
+
+    # add users to team
+    teams_with_users = [get_team_members(team) for team in WORKSHOP_TEAMS]
+
+    return render_template("teams.html", teams=teams_with_users)
 
 
 # only start web server if app.py is called directly;
