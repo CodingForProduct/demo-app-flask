0fb858a417450182e1803b075b297cc2e7f3eebe
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-01 05:19:28 -0700

    get the id from the url

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ def users():
     """display list of users"""
     return render_template("users.html", users=WORKSHOP_USERS)
 
+@app.route("/users/<id>")
+def user(id):
+    """display one user"""
+    return id
+
 # only start web server if app.py is called directly;
 # when file is called directly, __name__  is "__main__"
 if __name__ == "__main__":
