941d7364126f5ca99ae6ff5c3837ec027953e376
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-01 07:35:08 -0700

    9. display a list of teams

--- data/app_data.py
+++ data/app_data.py
@@ -1,8 +1,14 @@
 def users():
     return [
-        {"id": 1, "name": "Amy"},
-        {"id": 2, "name": "Bridget"},
-        {"id": 3, "name": "Carolina"},
-        {"id": 4, "name": "Christa"},
-        {"id": 5, "name": "Cindy"}
+        {"id": 1, "name": "Amy", "team_id": 1},
+        {"id": 2, "name": "Bridget", "team_id": 2},
+        {"id": 3, "name": "Carolina", "team_id": 2},
+        {"id": 4, "name": "Christa", "team_id": 1},
+        {"id": 5, "name": "Cindy", "team_id": 2}
+    ]
+
+def teams():
+    return [
+        {"id": 1, "name": "Team One"},
+        {"id": 2, "name": "Team Two"},
     ]
