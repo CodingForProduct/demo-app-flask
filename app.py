3d5b89ef14155413f188247121482e61c989cd75
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-01 05:07:14 -0700

    1. display "hi" at root route

---
+++ app.py
@@ -0,0 +1,19 @@
+"""This module runs the Flask app"""
+
+# libraries that this app needs
+from flask import Flask
+
+# initialize app
+# __name__ helps determine root path
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """display root route"""
+    return "hi"
+
+# only start web server if app.py is called directly;
+# when file is called directly, __name__  is "__main__"
+if __name__ == "__main__":
+    # start server
+    app.run()
