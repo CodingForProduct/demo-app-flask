30490979cf79705bfdd30bf2647bcee9383b24e2
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-01 05:10:22 -0700

    4. add users route and pass array of users to template

---
+++ data/app_data.py
@@ -0,0 +1,8 @@
+def users():
+    return [
+        {"id": 1, "name": "Amy"},
+        {"id": 2, "name": "Bridget"},
+        {"id": 3, "name": "Carolina"},
+        {"id": 4, "name": "Christa"},
+        {"id": 5, "name": "Cindy"}
+    ]
